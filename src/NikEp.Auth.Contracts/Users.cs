da9a00b0f454b82d6609e85f07405d2f16d520ee
Nikita Epifantsev <eppy_92@mail.ru>
2024-10-31 13:12:36 +0300

    Id # 2. Request models fixed

--- src/NikEp.Auth.Contracts/Users.cs
+++ src/NikEp.Auth.Contracts/Users.cs
@@ -1,8 +1,6 @@
-﻿using NikEp.Auth.Domain.ValueObjects;
-using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace NikEp.Auth.Contracts;
 
-public record RegisterUserRequest(string FirstName, string LastName, Email email, PhoneNumber number);
-public record EditUserRequest(Guid Id, string FirstName, string LastName, Email email, PhoneNumber number);
+public record RegisterUserRequest(string FirstName, string LastName, string email, string number);
+public record EditUserRequest(Guid Id, string FirstName, string LastName, string email, string number);
 public record RegisterUserResponse(Guid Id);
